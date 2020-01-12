8d8d5d13292722aad365c6b895ad24de1305fd4d
qnix95 <qnix95@nate.com>
2020-01-13 08:52:02 +0900

    Init

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+class Application {
+    constructor() {
+        console.log("Application Start.")
+    }
+}
+const app = new Application();
+
+/* WRITE DOWN CODE BELOW */
+
+
+
+/* END OF CODE */
+export default app;
